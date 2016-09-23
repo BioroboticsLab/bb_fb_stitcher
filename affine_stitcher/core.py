89d2b29651b997ffb164dc5ca29c6012226472cd
gitmirgut <gitmirgut@users.noreply.github.com>
2016-09-23 12:14:43 +0200

    work in progress

--- affine_stitcher/core.py
+++ affine_stitcher/core.py
@@ -1,3 +1,26 @@
 from logging import getLogger
+import affine_stitcher.rectificator as rect
+import affine_stitcher.rotator as rot
+import affine_stitcher.stitcher as stitch
+import affine_stitcher.helpers as helpers
 
 log = getLogger(__name__)
+
+class BB_FeatureBasedSticher(object):
+
+    def stitch(self, images):
+        (img_l, img_r) = images
+
+        re = rect.Rectificator()
+        img_l_re, img_r_re = re.rectify_images(img_l, img_r)
+
+        ro = rot.Rotator()
+        img_l_ro = ro.rotate_image(img_l, 90)
+        img_r_ro = ro.rotate_image(img_r, -90)
+        helpers.display(img_l_ro, 'img_l_ro')
+        helpers.display(img_r_ro, 'img_r_ro')
+        st =  stitch.FeatureBasedStitcher(overlap=400, affine=True)
+        homo, result, vis = st((img_l_ro, img_r_ro), True)
+        helpers.display(vis)
+        return result
+
---
+++ tests/test_core.py
@@ -0,0 +1,15 @@
+import logging.config
+import affine_stitcher.core as core
+import cv2
+import affine_stitcher.helpers as helpers
+
+logging.config.fileConfig('logging_config.ini')
+
+img_l =cv2.imread('data/core/Cam_0_20140918120038_045539_TO_Cam_0_20140918120622_267825.mkv.jpg')
+img_r =cv2.imread('data/core/Cam_1_20140918120522_949626_TO_Cam_1_20140918121058_677877.mkv.jpg')
+helpers.display(img_l,'left image', time=50)
+helpers.display(img_r, 'right image', time=50)
+bb_stitcher_fb = core.BB_FeatureBasedSticher()
+result = bb_stitcher_fb.stitch((img_l, img_r))
+helpers.display(result, 'left restult')
+# helpers.display(img_r_res, 'right result')
