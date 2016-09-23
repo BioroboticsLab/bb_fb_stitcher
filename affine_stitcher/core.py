3fe17a9c61c35a06b78efd3eb9755118034847b2
gitmirgut <gitmirgut@users.noreply.github.com>
2016-09-23 14:02:06 +0200

    work in progress

--- affine_stitcher/core.py
+++ affine_stitcher/core.py
@@ -3,6 +3,8 @@ import affine_stitcher.rectificator as rect
 import affine_stitcher.rotator as rot
 import affine_stitcher.stitcher as stitch
 import affine_stitcher.helpers as helpers
+from affine_stitcher.stitcher import Transformation
+
 
 log = getLogger(__name__)
 
@@ -19,7 +21,7 @@ class BB_FeatureBasedSticher(object):
         img_r_ro = ro.rotate_image(img_r, -90)
         helpers.display(img_l_ro, 'img_l_ro')
         helpers.display(img_r_ro, 'img_r_ro')
-        st =  stitch.FeatureBasedStitcher(overlap=400, affine=True)
+        st =  stitch.FeatureBasedStitcher(overlap=400, transformation=Transformation.PROJECTIVE)
         homo, result, vis = st((img_l_ro, img_r_ro), True)
         helpers.display(vis)
         return result
