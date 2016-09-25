c132f3c34bea9fcafe196b9eb5d55173a429796e
gitmirgut <gitmirgut@users.noreply.github.com>
2016-09-25 13:27:40 +0200

    add map coordinates, refactor image transformation, load and save arguments from/to file

--- affine_stitcher/core.py
+++ affine_stitcher/core.py
@@ -5,15 +5,18 @@ import affine_stitcher.stitcher as stitch
 from affine_stitcher.stitcher import Transformation
 import affine_stitcher.helpers as helpers
 import cv2
+import numpy as np
 
 log = getLogger(__name__)
 
 class BB_FeatureBasedStitcher(object):
 
     def __init__(self):
+        self.img_l_size = None
+        self.img_r_size = None
         self.whole_transform_left = None
         self.whole_transform_right = None
-        self.new_size = None
+        self.pano_size = None
         self.cached_img_l = None
         self.cached_img_r = None
 
@@ -32,29 +35,64 @@ class BB_FeatureBasedStitcher(object):
         self.whole_transform_left = img_l_ro_mat
         self.whole_transform_right = homo.dot(img_r_ro_mat)
 
-        trans_m , self.new_size = helpers.get_translation(self.cached_img_l.shape[:2], self.cached_img_r.shape[:2], self.whole_transform_left, self.whole_transform_right)
+        trans_m , self.pano_size = helpers.get_translation(self.cached_img_l.shape[:2], self.cached_img_r.shape[:2], self.whole_transform_left, self.whole_transform_right)
+        log.debug('new_size =\n{}'.format(self.pano_size))
 
         self.whole_transform_left = trans_m.dot(self.whole_transform_left)
         self.whole_transform_right = trans_m.dot(self.whole_transform_right)
 
         if pano:
             pano_img = st.warp_images()
-            return self.whole_transform_left, self.whole_transform_right, pano_img
+            return self.whole_transform_left, self.whole_transform_right, self.pano_size, pano_img
 
-        return self.whole_transform_left, self.whole_transform_right
+        return self.img_l_size, self.img_l_size, self.whole_transform_left, self.whole_transform_right, self.pano_size
+
+    @staticmethod
+    def transform_image(img, homography, pano_size):
+        re = rect.Rectificator()
+        img_rect = re.rectify_images(img)
+        trans_img = cv2.warpPerspective(img_rect, homography, pano_size)
+        return trans_img
 
     def transform_left_image(self, img=None):
         if img is None:
             img = self.cached_img_l
-        re = rect.Rectificator()
-        img_rect = re.rectify_images(img)
-        trans_img = cv2.warpPerspective(img_rect,self.whole_transform_left, self.new_size )
+        trans_img = BB_FeatureBasedStitcher.transform_image(img, self.whole_transform_left, self.pano_size)
         return trans_img
 
     def transform_right_image(self, img=None):
         if img is None:
             img = self.cached_img_r
-        re = rect.Rectificator()
-        img_rect = re.rectify_images(img)
-        trans_img = cv2.warpPerspective(img_rect, self.whole_transform_right, self.new_size )
+        trans_img = BB_FeatureBasedStitcher.transform_image(img, self.whole_transform_right, self.pano_size)
         return trans_img
+
+    @staticmethod
+    def map_coordinates(points, homography, org_size_img):
+        re = rect.Rectificator()
+        points_rect = re.rectify_points(points, org_size_img)
+        return cv2.transform(points_rect, homography)
+
+    def map_left_coordinates(self, points):
+        return self.map_coordinates(points, self.whole_transform_left, self.img_l_size)
+
+    def map_right_coordinates(self, points):
+        return self.map_coordinates(points, self.whole_transform_right, self.img_r_size)
+
+    def save_data(self, path):
+        np.savez(path,
+            img_l_size = self.img_l_size,
+            img_r_size = self.img_r_size,
+            whole_transform_left = self.whole_transform_left,
+            whole_transform_right = self.whole_transform_right,
+            pano_size = self.pano_size
+        )
+        log.info('Stitcher arguments saved to {}'.format(path))
+
+    def load_data(self, path):
+        with np.load(path) as data:
+            self.img_l_size = data['img_l_size']
+            self.img_r_size = data['img_r_size']
+            self.whole_transform_left = data['whole_transform_left']
+            self.whole_transform_right = data['whole_transform_right']
+            self.pano_size = data['pano_size']
+        log.info('Stitcher arguments loaded from {}'.format(path))
--- tests/test_core.py
+++ tests/test_core.py
@@ -17,7 +17,7 @@ img_r =cv2.imread('data/core/Cam_1_2016-07-19T12:41:22.685374Z--2016-07-19T12:47
 helpers.display(img_l,'left image', time=50)
 helpers.display(img_r, 'right image', time=50)
 bb_stitcher_fb = core.BB_FeatureBasedStitcher()
-__, __, result = bb_stitcher_fb((img_l, img_r), True)
+__, __, __, result = bb_stitcher_fb((img_l, img_r), True)
 helpers.display(result, 'left restult')
 
 img_l_alpha = cv2.cvtColor(img_l, cv2.COLOR_BGR2BGRA)
